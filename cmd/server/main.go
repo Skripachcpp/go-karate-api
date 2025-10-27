b4954770d4c5713fda32e9313aeb3ab03271ba45
Ружилов Артем <ruzhilov.a@promail366.com>
2025-10-28 00:56:57 +0300

    это была попытка подключить свагер

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,46 +1,47 @@
 package main
 
-// # Точка входа в приложение
-
 import (
-	"accounting-core/internal/handler"
-	"accounting-core/internal/repository"
-	"database/sql"
-	"log"
-	"net/http"
+    "accounting-core/internal/handler"
+    "accounting-core/internal/repository"
+    "database/sql"
+    "log"
+    "net/http"
 
-	_ "github.com/lib/pq"
+    _ "github.com/lib/pq"
 )
 
 func main() {
-	// Подключение к БД
-	connStr := "host=localhost port=5432 user=postgres password=password dbname=postgres sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+    // Подключение к БД
+    connStr := "host=localhost port=5432 user=postgres password=password dbname=postgres sslmode=disable"
+    db, err := sql.Open("postgres", connStr)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer db.Close()
 
-	if err := db.Ping(); err != nil {
-		log.Fatal("Cannot connect to DB:", err)
-	}
+    if err := db.Ping(); err != nil {
+        log.Fatal("Cannot connect to DB:", err)
+    }
 
-	// Инициализация репозитория и базы
-	repo := repository.NewPostgresRepo(db)
-	if err := repo.InitDB(); err != nil {
-		log.Fatal("Failed to init DB:", err)
-	}
-	if err := repo.SeedData(); err != nil {
-		log.Fatal("Failed to seed data:", err)
-	}
+    // Инициализация репозитория и базы
+    repo := repository.NewPostgresRepo(db)
+    if err := repo.InitDB(); err != nil {
+        log.Fatal("Failed to init DB:", err)
+    }
+    if err := repo.SeedData(); err != nil {
+        log.Fatal("Failed to seed data:", err)
+    }
 
-	// Инициализация хендлеров
-	accountHandler := handler.NewAccountHandler(repo)
+    // Инициализация хендлеров
+    accountHandler := handler.NewAccountHandler(repo)
 
-	// Маршруты
-	http.HandleFunc("/accounts", accountHandler.GetAccounts)
-	http.HandleFunc("/transfer", accountHandler.Transfer)
+    // Маршруты
+    http.HandleFunc("/accounts", accountHandler.GetAccounts)
+    http.HandleFunc("/transfer", accountHandler.Transfer)
 
-	log.Println("Server starting on :8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Println("Server starting on :8080...")
+    log.Println("API endpoints:")
+    log.Println("  GET  http://localhost:8080/accounts")
+    log.Println("  POST http://localhost:8080/transfer")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
