490ed873a3fed87d36af78a1e86bff081c1b96eb
Ружилов Артем <ruzhilov.a@promail366.com>
2025-10-28 01:22:23 +0300

    за полтора часа может и норм, надо причесать

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,47 +1,64 @@
 package main
 
 import (
-    "accounting-core/internal/handler"
-    "accounting-core/internal/repository"
-    "database/sql"
-    "log"
-    "net/http"
+	"accounting-core/internal/handler"
+	"accounting-core/internal/repository"
+	"database/sql"
+	"log"
+	"net/http"
 
-    _ "github.com/lib/pq"
+	_ "github.com/lib/pq"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
+// @title Accounting Core API
+// @version 1.0
+
+// @description Бухгалтерское ядро банка на Go
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+
+// @host localhost:8080
+// @BasePath /
 func main() {
-    // Подключение к БД
-    connStr := "host=localhost port=5432 user=postgres password=password dbname=postgres sslmode=disable"
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer db.Close()
+	// Подключение к БД
+	connStr := "host=localhost port=5432 user=postgres password=password dbname=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-    if err := db.Ping(); err != nil {
-        log.Fatal("Cannot connect to DB:", err)
-    }
+	if err := db.Ping(); err != nil {
+		log.Fatal("Cannot connect to DB:", err)
+	}
 
-    // Инициализация репозитория и базы
-    repo := repository.NewPostgresRepo(db)
-    if err := repo.InitDB(); err != nil {
-        log.Fatal("Failed to init DB:", err)
-    }
-    if err := repo.SeedData(); err != nil {
-        log.Fatal("Failed to seed data:", err)
-    }
+	// Инициализация репозитория и базы
+	repo := repository.NewPostgresRepo(db)
+	if err := repo.InitDB(); err != nil {
+		log.Fatal("Failed to init DB:", err)
+	}
+	if err := repo.SeedData(); err != nil {
+		log.Fatal("Failed to seed data:", err)
+	}
 
-    // Инициализация хендлеров
-    accountHandler := handler.NewAccountHandler(repo)
+	// Инициализация хендлеров
+	accountHandler := handler.NewAccountHandler(repo)
 
-    // Маршруты
-    http.HandleFunc("/accounts", accountHandler.GetAccounts)
-    http.HandleFunc("/transfer", accountHandler.Transfer)
+	// Маршруты
+	http.HandleFunc("/accounts", accountHandler.GetAccounts)
+	http.HandleFunc("/transfer", accountHandler.Transfer)
 
-    log.Println("Server starting on :8080...")
-    log.Println("API endpoints:")
-    log.Println("  GET  http://localhost:8080/accounts")
-    log.Println("  POST http://localhost:8080/transfer")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	// Swagger UI
+	http.HandleFunc("/swagger/", httpSwagger.WrapHandler)
+
+	log.Println("Server starting on :8080...")
+	log.Println("Swagger UI: http://localhost:8080/swagger/index.html")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- internal/handler/account.go
+++ internal/handler/account.go
@@ -15,6 +15,15 @@ func NewAccountHandler(repo *repository.PostgresRepo) *AccountHandler {
     return &AccountHandler{repo: repo}
 }
 
+// GetAccounts godoc
+// @Summary Get all accounts
+// @Description Get list of all bank accounts
+// @Tags accounts
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} domain.Account
+// @Failure 500 {object} map[string]string
+// @Router /accounts [get]
 func (h *AccountHandler) GetAccounts(w http.ResponseWriter, r *http.Request) {
     accounts, err := h.repo.GetAccounts()
     if err != nil {
@@ -26,6 +35,17 @@ func (h *AccountHandler) GetAccounts(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(accounts)
 }
 
+// Transfer godoc
+// @Summary Transfer money between accounts
+// @Description Make a money transfer from one account to another
+// @Tags transactions
+// @Accept  json
+// @Produce  json
+// @Param transaction body domain.Transaction true "Transfer data"
+// @Success 200 {object} map[string]string
+// @Failure 400 {object} map[string]string
+// @Failure 500 {object} map[string]string
+// @Router /transfer [post]
 func (h *AccountHandler) Transfer(w http.ResponseWriter, r *http.Request) {
     var req domain.Transaction
     if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
