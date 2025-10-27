19832bb3329ab856b859f2bd22695786cfd9ebf2
Ружилов Артем <ruzhilov.a@promail366.com>
2025-10-28 00:44:14 +0300

    разложил по папкам

---
+++ internal/domain/account.go
@@ -0,0 +1,13 @@
+package domain
+
+type Account struct {
+    ID      int64   `json:"id"`
+    Number  string  `json:"number"`
+    Balance float64 `json:"balance"`
+}
+
+type Transaction struct {
+    DebitAccountID  int64   `json:"debit_account_id"`
+    CreditAccountID int64   `json:"credit_account_id"`
+    Amount          float64 `json:"amount"`
+}
